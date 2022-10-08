4f16730011a918cc99a023577f64b0d3e49a8dac
Gabe Grieve <55262295+gabe-newbeach@users.noreply.github.com>
2022-10-09 10:36:29 +1100

    part 1.c tutorial

--- part1/tutorial/src/App.js
+++ part1/tutorial/src/App.js
@@ -1,22 +1,23 @@
-const Hello = (props) => (
-  <>
-    <p>
-      Hello {props.name}, you are {props.age} years old.
-    </p>
-  </>
-);
+import { useState } from "react";
 
 const App = () => {
-  const name = "Bingo";
-  const age = 8;
+  const [counter, setCounter] = useState(0);
+  const increaseCounter = () => setCounter(counter + 1);
+  const decreaseCounter = () => setCounter(counter - 1);
+  const resetCounter = () => setCounter(0);
 
   return (
     <>
-      <p>Greetings</p>
-      <Hello name="Gary" age={20 + 11} />
-      <Hello name={name} age={age} />
+      <Display counter={counter} />
+      <Button onClick={increaseCounter} text="Increase Counter" />
+      <Button onClick={resetCounter} text="Reset Counter" />
+      <Button onClick={decreaseCounter} text="Decrease Counter" />
     </>
   );
 };
 
 export default App;
+
+const Display = ({ counter }) => <div>The counter is set to {counter}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
