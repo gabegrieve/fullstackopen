5105bf0a7ac8c1588cea7e81d40fdaf9a3ee24df
Gabe Grieve <55262295+gabe-newbeach@users.noreply.github.com>
2022-10-11 18:13:23 +1100

    exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Heading title="Give Feedback" />
+      <Button onClick={increaseGood} text="Good" />
+      <Button onClick={increaseNeutral} text="Neutral" />
+      <Button onClick={increaseBad} text="Bad" />
+      <Heading title="Statistics" />
+      <Results good={good} neutral={neutral} bad={bad} />
+    </>
   );
-}
+};
 
 export default App;
+
+const Heading = ({ title }) => <h2>{title}</h2>;
+
+const Button = ({ onClick, text }) => {
+  return (
+    <>
+      <button onClick={onClick}>{text}</button>
+    </>
+  );
+};
+
+const Results = ({ good, bad, neutral }) => {
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <td>Good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>Neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>Bad</td>
+            <td>{bad}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
