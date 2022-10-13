b660727747d69b243fc2763c3c91deb60fa363db
Gabe Grieve <55262295+gabe-newbeach@users.noreply.github.com>
2022-10-13 17:07:09 +1100

    change state of data to objects

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,26 +1,54 @@
 import { useState } from "react";
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+
+  const quotes = [
+    {
+      anecdote: "If it hurts, do it more often.",
+      votes: 0,
+    },
+    {
+      anecdote: "Adding manpower to a late software project makes it later!",
+      votes: 0,
+    },
+    {
+      anecdote:
+        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+      votes: 0,
+    },
+    {
+      anecdote:
+        "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+      votes: 0,
+    },
+    {
+      anecdote: "Premature optimization is the root of all evil.",
+      votes: 0,
+    },
+    {
+      anecdote:
+        "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+      votes: 0,
+    },
+    {
+      anecdote:
+        "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+      votes: 0,
+    },
   ];
 
-  const [selected, setSelected] = useState(0);
-  const [quote, setQuote] = useState(anecdotes[0]);
+  console.log(quotes[1].anecdote);
+
+  const [quote, setQuote] = useState(quotes[0].anecdote);
 
   const generateRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
   const selectQuote = () => {
-    let index = anecdotes[generateRandomNumber(1, anecdotes.length - 1)];
-    setQuote(index);
+    let selectedQuote =
+      quotes[generateRandomNumber(0, quotes.length - 1)].anecdote;
+    setQuote(selectedQuote);
   };
 
   return (
