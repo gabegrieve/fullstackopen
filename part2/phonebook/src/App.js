8df42a51ed1981da5b6fa2baf22f6d95134717e1
Gabe Grieve <55262295+gabe-newbeach@users.noreply.github.com>
2022-10-17 20:41:56 +1100

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,16 @@ import { useState } from "react";
 
 const App = () => {
   const phonebookRecords = [
-    { name: "Arto Hellas", number: "0415987635", id: 1 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ];
   const [persons, setPersons] = useState(phonebookRecords);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState(phonebookRecords);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -14,6 +19,19 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (value) => {
+    let searchResults = persons.filter((person) =>
+      person.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setSearchResults(searchResults);
+  };
+
+  const handleQueryChange = (event) => {
+    let value = event.target.value;
+    setSearchQuery(value);
+    handleSearch(value);
   };
 
   const addName = (event) => {
@@ -34,6 +52,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter by name:{" "}
+        <input value={searchQuery} onChange={handleQueryChange} />
+      </div>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -49,7 +71,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {searchResults.map((person) => (
           <li key={person.name}>
             {person.name} {person.number}
           </li>
