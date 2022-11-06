77da8d740bb6fb1ff1c953ee6a533aa1a1060326
Gabe Grieve <55262295+gabe-newbeach@users.noreply.github.com>
2022-11-06 15:15:15 +1100

    4.4: helper functions and unit tests, step2

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -9,7 +9,25 @@ const totalLikes = (blogs) => {
   return total;
 };
 
+const favouriteBlog = (blogs) => {
+  const reducer = (previousBlog, currentBlog) => {
+    if (currentBlog.likes >= previousBlog.likes) {
+      return currentBlog;
+    } else {
+      return previousBlog;
+    }
+  };
+  const reducedOutput = blogs.reduce(reducer);
+  const formattedBlog = {
+    title: reducedOutput.title,
+    author: reducedOutput.author,
+    likes: reducedOutput.likes,
+  };
+  return formattedBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
